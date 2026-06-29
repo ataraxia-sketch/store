1c01af9e9b61f7bf0368b670292d91d8e8d0e7c3
atara_xia <ataraxiaofficial2026@gmail.com>
2026-06-30 03:46:24 +0700

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFf6F6EU9h4hKwViE_xu0Re1_wqu7FqdQ",
+  authDomain: "ataraxia-2026.firebaseapp.com",
+  projectId: "ataraxia-2026",
+  storageBucket: "ataraxia-2026.firebasestorage.app",
+  messagingSenderId: "617393566426",
+  appId: "1:617393566426:web:490330b7719d45f5ceebe3",
+  measurementId: "G-1VX3V0VDWM"
+};
